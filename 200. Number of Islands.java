6e2d64a129cfb9b33cb5dc331df5a4ead59c3c94
Vu Nguyen <vu@vnguyen.io>
2016-10-19 23:25:24 -0400

    Create 200. Number of Islands.java

---
+++ 200. Number of Islands.java
@@ -0,0 +1,51 @@
+public class Solution {
+    public int numIslands(char[][] grid) {
+        int width = grid[0].length;
+        int height = grid.length;
+        int numOfIslands = 0;
+        int[][] visited = new int[height][width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++){
+                if (grid[i][j] == '1' && visited[i][j] == 0) {
+                    boolean island = false;
+                    ArrayList<Integer> nextR = new ArrayList<>();
+                    nextR.add(i);
+                    ArrayList<Integer> nextC = new ArrayList<>();
+                    nextC.add(j);
+                    
+                    while (nextR.size() > 0) {
+                        int r = nextR.remove(0);
+                        int c = nextC.remove(0);
+
+                        if (visited[r][c] == 0) {
+                            visited[r][c] = 1;
+
+                            if (island == false) {
+                                numOfIslands++;
+                                island = true;
+                            }
+                            if ((r - 1 >= 0 && r - 1 < height) && grid[r-1][c] == '1') {
+                                nextR.add(r-1);
+                                nextC.add(c);
+                            }
+                            if ((r + 1 >= 0 && r + 1 < height) && grid[r+1][c] == '1') {
+                                nextR.add(r+1);
+                                nextC.add(c);
+                            }
+                            if ((c - 1 >= 0 && c - 1 < width) && grid[r][c-1] == '1') {
+                                nextR.add(r);
+                                nextC.add(c-1);
+                            }
+                            if ((c + 1 >= 0 && c + 1 < width) && grid[r][c+1] == '1') {
+                                nextR.add(r);
+                                nextC.add(c+1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return numOfIslands;
+    }
+}
