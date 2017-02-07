b9ab3a425958026eaa195e2c2b3a3a8b7aeffa6f
Vu Nguyen <vu@vnguyen.io>
2017-02-07 14:52:42 -0500

    Update 448. Find All Numbers Disappeared in an Array.java

--- 448. Find All Numbers Disappeared in an Array.java
+++ 448. Find All Numbers Disappeared in an Array.java
@@ -19,3 +19,20 @@ public class Solution {
         return out;
     }
 }
+
+// Consider working directly on the input to achieve O(1) space
+List<Integer> result = new ArrayList<Integer>();
+    for( int i=0;i< nums.length; i++){
+        int index = nums[i];
+        if(nums[Math.abs(index)-1] > 0){
+            nums[Math.abs(index)-1]= -nums[Math.abs(index)-1];
+        }
+    }
+
+    for(int j =1 ;j <= nums.length ; j++){
+        if(nums[j-1] > 0){
+            result.add(j);
+        }
+    }
+    return result;
+}
