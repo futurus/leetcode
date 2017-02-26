fd5a9e3cbe2de83c451ab7f0610e6ab16a848396
Vu Nguyen <futurus@users.noreply.github.com>
2017-02-26 01:23:43 -0500

    Create 022. Generate Parentheses.java

---
+++ 022. Generate Parentheses.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public void helper(List<String> result, int n, String sofar, int left, int right) {
+        if (sofar.length() == n*2) {
+            result.add(sofar);
+            return;
+        }
+        
+        if (left == right) {
+            helper(result, n, sofar + "(", left + 1, right);
+        } else if (left > right && left < n) {
+            helper(result, n, sofar + "(", left + 1, right);
+            helper(result, n, sofar + ")", left, right + 1);
+        } else {
+            helper(result, n, sofar + ")", left, right + 1);
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> out = new ArrayList<String>();
+        helper(out, n, "", 0, 0);
+        
+        return out;
+    }
+}
