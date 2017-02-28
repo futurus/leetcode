e3e74879afd0ec48a35f746337bc68c2d3952844
Vu Nguyen <futurus@users.noreply.github.com>
2017-02-28 13:42:47 -0500

    Update 088. Merge Sorted Array.java

--- 088. Merge Sorted Array.java
+++ 088. Merge Sorted Array.java
@@ -15,3 +15,22 @@ public class Solution {
         return;
     }
 }
+
+// even more succint
+// 1st loop: either i runs out first or j runs out first
+// - if i runs out first, finish with elems from j
+// - if j runs out first, 2nd loop won't be executed, won't need to 
+//   move the remaining elements from first (also resulting) array either
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int c = m + n, i = m - 1, j = n - 1;
+        
+        while (i >= 0 && j >= 0) 
+            nums1[--c] = nums1[i] <= nums2[j] ? nums2[j--] : nums1[i--];
+        
+        while (j >= 0)
+            nums1[--c] = nums2[j--];
+       
+        return;
+    }
+}
