e4a87e70ce35ce9fa641aa290523c5195ad9dfcb
Vu Nguyen <futurus@users.noreply.github.com>
2017-03-02 17:05:25 -0500

    Update 012. Integer to Roman.java

--- 012. Integer to Roman.java
+++ 012. Integer to Roman.java
@@ -1,4 +1,4 @@
-// much inelegant!
+// much inelegant! but extensible
 
 // https://leetcode.com/problems/integer-to-roman
 public class Solution {
@@ -37,4 +37,15 @@ public class Solution {
         }
         return out.toString();
     }
+}
+
+
+// quickie, however not extensible
+// https://discuss.leetcode.com/topic/39799/simple-java-solution
+public String intToRoman(int num) {
+    String M[] = {"", "M", "MM", "MMM"};
+    String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    return M[num/1000] + C[(num%1000)/100]+ X[(num%100)/10] + I[num%10];
 }
