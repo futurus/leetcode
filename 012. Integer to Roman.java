99ad33934d2a0aef405b9f4ebc6a29ef038c7669
Vu Nguyen <futurus@users.noreply.github.com>
2017-03-02 17:06:18 -0500

    Update 012. Integer to Roman.java

--- 012. Integer to Roman.java
+++ 012. Integer to Roman.java
@@ -42,10 +42,14 @@ public class Solution {
 
 // quickie, however not extensible
 // https://discuss.leetcode.com/topic/39799/simple-java-solution
-public String intToRoman(int num) {
-    String M[] = {"", "M", "MM", "MMM"};
-    String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    return M[num/1000] + C[(num%1000)/100]+ X[(num%100)/10] + I[num%10];
+public class Solution {
+    private static String M[] = {"","M","MM","MMM"};
+    private static String C[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+    private static String X[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+    private static String I[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+    
+    public String intToRoman(int num) {
+	    StringBuilder roman = new StringBuilder();
+	    return roman.append(M[num/1000]).append(C[(num%1000)/100]).append(X[(num%100)/10]).append(I[num%10]).toString();
+    }
 }
