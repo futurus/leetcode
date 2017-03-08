5839ac2d2d128efcf43cadbd1e647cd37f3366a7
Vu Nguyen <futurus@users.noreply.github.com>
2017-03-07 21:49:05 -0500

    Update 053. Maximum Subarray.java

--- 053. Maximum Subarray.java
+++ 053. Maximum Subarray.java
@@ -42,3 +42,40 @@ public class Solution {
         }
     }
 }
+
+
+// improvement, still failed
+
+public class Solution {
+    public int sum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+    
+    public int helper(int[] nums) {
+        int max = sum(nums);
+
+        for (int div = 1; div <= nums.length - 1; div++) {
+            int val = Math.max(helper(Arrays.copyOfRange(nums, 0, div)), 
+                               helper(Arrays.copyOfRange(nums, div, nums.length)));
+            if (val >= max) {
+                max = val;
+            }
+        }
+        
+        return max;
+    }
+    
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        } else {
+            return helper(nums);
+        }
+    }
+}
