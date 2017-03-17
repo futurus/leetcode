3f3f1b90e31fb7bdced3cc0244c3f5d7d9a060b7
Vu Nguyen <futurus@users.noreply.github.com>
2017-03-17 16:16:20 -0400

    Create 096. Unique Binary Search Trees.java

---
+++ 096. Unique Binary Search Trees.java
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/unique-binary-search-trees
+// Dynamic Programming!!!
+// see https://discuss.leetcode.com/topic/8398/dp-solution-in-6-lines-with-explanation-f-i-n-g-i-1-g-n-i
+
+public class Solution {
+    public int numTrees(int n) {
+        int [] G = new int[n+1];
+        G[0] = G[1] = 1;
+        
+        for(int i=2; i<=n; ++i) {
+        	for(int j=1; j<=i; ++j) {
+        		G[i] += G[j-1] * G[i-j];
+        	}
+        }
+    
+        return G[n];
+    }
+}
